0302f5d314cffa270c70de1f1241649a8c88ceba
sojeongsojeong-park <psj90972@naver.com>
2025-03-01 13:34:20 +0900

    fix: fix introduction content

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,36 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 import {
-  About,
-  Contact,
-  Experience,
-  Feedbacks,
-  Hero,
-  Navbar,
-  StarsCanvas,
-  Tech,
-  Works,
-} from "./components";
+	About,
+	Contact,
+	Experience,
+	Hero,
+	Navbar,
+	StarsCanvas,
+	Tech,
+	Works,
+} from './components';
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-          <Navbar />
-          <Hero />
-          <div>
-            <About />
-            <Experience />
-            <Tech />
-            <Works />
-            <Feedbacks />
-            <div className='relative z-0'>
-              <Contact />
-              <StarsCanvas />
-            </div>
-          </div>
-        </div>
-      </div>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<div className='relative z-0 bg-primary'>
+				<div className='bg-center bg-no-repeat bg-cover bg-hero-pattern'>
+					<Navbar />
+					<Hero />
+					<div>
+						<About />
+						<Experience />
+						<Tech />
+						<Works />
+						<div className='relative z-0'>
+							<Contact />
+							<StarsCanvas />
+						</div>
+					</div>
+				</div>
+			</div>
+		</BrowserRouter>
+	);
 };
 
 export default App;
